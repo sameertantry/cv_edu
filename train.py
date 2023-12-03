8f909edabac591de7f2283abebe7a97f4818d823
sameertantry <sameertantry@gmail.com>
2023-12-03 04:51:01 +0300

    flowers dataset uploaded to gdrive using dvc

--- train.py
+++ train.py
@@ -1,24 +1,4 @@
 import torch
-from omegaconf import DictConfig, OmegaConf
-from src.models.lenet import LeNet
-from src.utils import load_splits
 
 
-splits = load_splits("data/flowers/tiny_splits.csv")
-config = OmegaConf.load("configs/classification_config.yaml")
-
-model = LeNet(config.model)
-
-
-torch.save(
-    {"model_state_dict": model.state_dict()},
-    "lenet.pth",
-)
-
-
-def train(config: DictConfig):
-    pass
-
-
-if __name__ == "__main__":
-    config = OmegaConf.load("configs/classification_config.yaml")
+x = torch.zeros(1)
